e2ce41cb9f29c4c959b089b1cae0eda35ea3e8b2
navono <navono007@gmail.com>
2019-03-28 12:43:43 +0800

    fix: init project with CRA 2.0 and --typescript

--- pkg/base/src/App.tsx
+++ pkg/base/src/App.tsx
@@ -1,71 +1,29 @@
-import * as React from 'react';
-import { Button, Layout, Menu, Icon } from 'antd';
-import DocumentTitle from 'react-document-title';
+import React, { Component } from 'react';
+import { Button } from 'antd';
+import logo from './logo.svg';
 import './App.css';
 
-const { Header, Sider, Content } = Layout;
-
-class App extends React.Component {
-  public state = {
-    collapsed: false,
-  }
-
-  public componentDidCatch(error: any, info: any) {
-    console.log(error, info);
-  }
-
-  public render() {
+class App extends Component {
+  render() {
     return (
       <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
         <Button type="primary">Button</Button>
-        <DocumentTitle title={"Test Tile"}>
-          <Layout>
-            <Sider
-              trigger={null}
-              collapsible={true}
-              collapsed={this.state.collapsed}
-            >
-              <div className="logo" />
-              <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                <Menu.Item key="1">
-                  <Icon type="user" />
-                  <span>nav 1</span>
-                </Menu.Item>
-                <Menu.Item key="2">
-                  <Icon type="video-camera" />
-                  <span>nav 2</span>
-                </Menu.Item>
-                <Menu.Item key="3">
-                  <Icon type="upload" />
-                  <span>nav 3</span>
-                </Menu.Item>
-              </Menu>
-            </Sider>
-            <Layout>
-              <Header style={{ background: '#fff', padding: 0 }}>
-                <Icon
-                  className="trigger"
-                  type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-                  onClick={this.toggle}
-                />
-              </Header>
-              <Content style={{
-                margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280,
-              }}
-              >
-                Content
-              </Content>
-            </Layout>
-          </Layout>
-        </DocumentTitle>
       </div>
     );
-  }
-
-  private toggle  = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
   }
 }
 
